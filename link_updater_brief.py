ac60321baa0e3674a9490095c3f60ffb2de73ac3
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-01 23:23:46 +0530

    briefs and market
    added link updater and database for finshot market and brief

---
+++ link_updater_brief.py
@@ -0,0 +1,55 @@
+"""his script when run will extract article links from the Finshots website
+ and update the links table in database"""
+
+import datetime
+import os
+
+import mysql.connector as mc
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+# making the connection to database
+load_dotenv()
+User = os.getenv('USER')
+Host = os.getenv('HOST')
+Password = os.getenv('PASSWORD')
+Database = os.getenv('DATABASE')
+
+db = mc.connect(user=User, host=Host, password=Password, database=Database)
+cur = db.cursor()
+
+# scrapping links form the finshots website
+url = "https://finshots.in/brief/"
+r = requests.get(url).content
+
+soup = BeautifulSoup(r, 'html.parser')
+div = soup.find('div', class_='post-feed')
+briefs = div.find_all('article')
+
+# updating new brief links in the database
+for item in briefs:
+    brief = {
+        'link_b': "https://finshots.in" + item.find('a')['href'],
+        'title_b': item.find('img')['alt'],
+        'link_date_b': item.find('time')['datetime']}
+    now = datetime.datetime.now().strftime(r"%Y:%m:%d %H:%M:%S")
+
+    try:
+        cur.execute(
+            f"insert into brief values('{brief['link_b']}',"
+            f"'{brief['title_b']}','{brief['link_date_b']}','{now}');")
+        db.commit()
+    except (mc.errors.IntegrityError, mc.errors.ProgrammingError):
+        pass
+
+print('database updated with fresh new briefs!')
+
+# deleting articles older than 7 days from the database
+cur.execute(
+    'delete from articles where timestampdiff(day, link_date, curdate())>2 ;')
+db.commit()
+
+# closing connection to the database
+cur.close()
+db.close()
