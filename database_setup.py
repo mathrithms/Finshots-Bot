49fe91cef69c94236dca543af138914adba53f52
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-02 19:51:45 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -46,10 +46,7 @@ cur.execute(
 
 cur.execute(
     'create table infographics (img varchar(200) primary key, title'
-    'varchar(200), link_date date, update_time datetime);'
-
-
-
+    'varchar(200), link_date date, update_time datetime);')
 
 
 print('Database created succesfully')
