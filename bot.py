6d33c87e8762f6b53a7228c94be02c3f49902df5
NiveditJain <jain.22@iitj.ac.in>
2021-03-09 10:53:18 +0530

    bug fixed

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 """this is the file that will run 24*7 on the server"""
 
 import asyncio
-# import datetime
+import datetime
 import os
 import random
 
@@ -53,15 +53,14 @@ async def on_ready():
 
         # extracting the articles that are updated in the database withing
         # past 24 hours from now
-        # now = datetime.datetime.now().strftime(r"%Y:%m:%d %H:%M:%S")
+        now = datetime.datetime.now().strftime(r"%Y:%m:%d %H:%M:%S")
 
         category = ['daily', 'brief', 'markets', 'infographics']
         articles = []
         for value in category:
             cur.execute(
                 "select links, title, category, link_date from articles "
-                f"where link_date=curdate() and"
-                f" category='{value}';")
+                f"where category='{value}' and timestampdiff(minute, update_time, '{now}') < 1440;")
             articles += cur.fetchall()
 
         # sending the articles in the required channels
